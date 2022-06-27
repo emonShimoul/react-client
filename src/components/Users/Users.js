bcbfb0718da848b9120a59b1da1190e112c57be9
emonShimoul <emonshimoul@gmail.com>
2022-06-27 16:13:49 +0600

    display data from mongodb on the UI

--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -14,7 +14,9 @@ const Users = () => {
         <div>
             <h2>Users:</h2>
             <ul>
-                
+                {
+                    users.map(user => <li>{user.name} - {user.email}</li>)
+                }
             </ul>
         </div>
     );
