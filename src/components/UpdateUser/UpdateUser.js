0232d8e653667c237181f3a676298dd32e7a828d
emonShimoul <emonshimoul@gmail.com>
2022-06-28 13:55:54 +0600

    setup update form to update an user

--- src/components/UpdateUser/UpdateUser.js
+++ src/components/UpdateUser/UpdateUser.js
@@ -14,11 +14,20 @@ const UpdateUser = () => {
 
     // update user
     const handleNameChange = e => {
-        
+        const updatedName = e.target.value;
+        const updatedUser = {name: updatedName, email: user.email};
+        setUser(updatedUser);
     }
 
     const handleEmailChange = e => {
+        const updatedEmail = e.target.value;
+        // const updatedUser = {name: user.name, email: updatedEmail};
 
+        // fancy system
+        const updatedUser = {...user};
+        updatedUser.email = updatedEmail;
+        
+        setUser(updatedUser);
     }
 
     const handleUpdateUser = e => {
