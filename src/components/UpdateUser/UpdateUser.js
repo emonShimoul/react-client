e81c6f4ec718f008eb7e35e90cbc7a5029a5440e
emonShimoul <emonshimoul@gmail.com>
2022-06-28 13:23:44 +0600

    load data for specific user from mongodb

--- src/components/UpdateUser/UpdateUser.js
+++ src/components/UpdateUser/UpdateUser.js
@@ -4,6 +4,13 @@ import { useParams } from 'react-router-dom';
 const UpdateUser = () => {
     const {id} = useParams();
 
+    useEffect(() => {
+        const url = `http://localhost:5000/users/${id}`;
+        fetch(url)
+        .then(res => res.json())
+        .then(data => console.log(data))
+    }, []);
+
     return (
         <div>
             <h2>Update User:</h2>
